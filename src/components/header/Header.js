3379d7684da158173ec30f746b551cabb1ad4888
Ivan <susnicaivan90@gmail.com>
2022-05-22 09:35:12 +0200

    mobile responsiveness adjustment header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ function Header (props) {
     return (
       <header>
         <h1>YT CLONE</h1>
-        <div>
+        <div className="search">
             <input type="text" name="" id="searchInput" placeholder="type to search..." onChange={(e)=>props.setSearchQuery(e.target.value) } />
             <button onClick={()=>props.update()}>Search</button>
         </div>
